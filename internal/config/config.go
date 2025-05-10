17f7a1549341f65edab5537774b03e8ed5d8803d
Yuriy Malyshev <100198677+YuarenArt@users.noreply.github.com>
2025-05-10 17:23:44 +0300

    Add LogType, LogToConsole, LogToFile, LogFile for Config

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,15 +7,21 @@ import (
 
 // Config contains settings for running the PubSub gRPC service.
 type Config struct {
-	GRPCAddr string // Address and port for the gRPC service (e.g., "localhost:50051")
-	LogLevel string // Logging level (e.g., "debug", "info", "warn", "error")
+	GRPCAddr     string // Address and port for the gRPC service (e.g., "localhost:50051")
+	LogType      string // Logger type (e.g., "slog")
+	LogToConsole string // Enable logging to console (e.g., "true", "Y", "T", "1")
+	LogToFile    string // Enable logging to file (e.g., "true", "Y", "T", "1")
+	LogFile      string // Log file path (e.g., "logs/server.log")
 }
 
 // NewConfig initializes the configuration, prioritizing environment variables, then command-line flags, then defaults.
 func NewConfig() *Config {
 	return &Config{
-		GRPCAddr: configValue("GRPC_ADDRESS", "a", "localhost:50051", "address and port to run gRPC service"),
-		LogLevel: configValue("LOG_LEVEL", "l", "info", "logging level (debug, info, warn, error)"),
+		GRPCAddr:     configValue("GRPC_ADDRESS", "a", "localhost:50051", "address and port to run gRPC service"),
+		LogType:      configValue("LOG_TYPE", "l", "slog", "logger type (e.g., slog)"),
+		LogFile:      configValue("LOG_FILE", "logfile", "logs/pubsub.log", "log file path"),
+		LogToConsole: configValue("LOG_TO_CONSOLE", "logconsole", "true", "enable logging to console (true, Y, T, 1)"),
+		LogToFile:    configValue("LOG_TO_FILE", "logfileenable", "true", "enable logging to file (true, Y, T, 1)"),
 	}
 }
 
@@ -24,7 +30,7 @@ func NewConfig() *Config {
 // 2. Command-line flag.
 // 3. Default value.
 func configValue(envVar, flagName, defaultValue, description string) string {
-	// Read environment variable.
+
 	envValue := os.Getenv(envVar)
 	if envValue != "" {
 		return envValue
